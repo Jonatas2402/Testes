b6ebbd493b1b0185f650f9b2ce5d8ac691ae4546
jonatas <jonatas_dev@hotmail.com>
2026-03-30 16:34:19 -0300

    criando metodos

--- src/main/java/com/testes/API/controller/PlanetController.java
+++ src/main/java/com/testes/API/controller/PlanetController.java
@@ -3,6 +3,7 @@ package com.testes.API.controller;
 import com.testes.API.model.Planet;
 import com.testes.API.service.PlanetService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,5 +23,8 @@ public class PlanetController {
         return ResponseEntity.status(HttpStatus.CREATED).body(create);
     }
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void>
+    public ResponseEntity<Void> deletePorId(Long id){
+        service.deletarPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }
