37863e5e8483d0301fb25df723939246738fcb5f
jonatas <jonatas_dev@hotmail.com>
2026-04-01 19:25:45 -0300

    Primeiro teste com o mockito

--- src/test/java/com/testes/API/domain/PlanetServiceTest.java
+++ src/test/java/com/testes/API/domain/PlanetServiceTest.java
@@ -1,10 +1,14 @@
 package com.testes.API.domain;
-import static com.testes.API.common.PlanetConstants.PLANET;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static com.testes.API.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
 
 /*Padrão de nomeclatura dos testes
  * 1° Operação = A operação que está sendo testada
@@ -17,9 +21,16 @@ public class PlanetServiceTest {
 
     @Autowired
     private PlanetService planetService;
+    @MockitoBean
+    private PlanetRepository repository;
     //operacao_estado_retorno.
     @Test
     public void creatPlanet_withValidData_returnsPlanet(){
+        /*Quando o repository for chamado no método salvar com exatamente o planeta
+        * o retorno esperado deve ser ele mesmo. */
+        /* AAA
+        * A = Arrange*/
+        when(repository.save(PLANET)).thenReturn(PLANET);
         //O objeto sob teste é chamado de sut.
         //System under test
        Planet sut = planetService.criandoPlaneta(PLANET);
